2644e3dedaaccdb20e4719441a0da1a2412374a6
CELIA <c.martinher@outlook.es>
2017-04-03 17:31:31 +0200

    Ejercicio resuelto

---
+++ ajax.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $.ajax({
+      type:"GET:",
+      url: "text.txt",
+      cache:false
+    }).done(function(text){
+      $("#second").html(text);
+    });
+    $("#second").clcik(function(){
+      $.ajax({
+        type:"GET",
+        url: "text1.txt",
+        cache:false
+      }).done(function(text){
+        $("#third").html(text);
+      });
+    });
+    $("#fourth").click(function(){
+      var request = $.ajax({
+        type:"GET",
+        url: "text2.txt", //este texto no existe con lo cual habra un done y un fail
+        cache:false
+    });
+    request.done(function(text){
+      $("#fiveth").html(text);
+    });
+    request.fail(function(text){
+      $("#fiveth").html("error al coger text");
+    });
+  });
